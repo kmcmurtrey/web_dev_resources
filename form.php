55b499a920c645754293b89259ecddf28cdb161e
Korey McMurtrey <kmcmurtrey@gmail.com>
2015-06-16 19:01:58 -0400

    Create form.php with form elements

---
+++ form.php
@@ -0,0 +1,21 @@
+<form method="post" class="website_form">
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="" value="">
+    </div>
+    
+    <div>
+        <label for="url">URL</label>
+        <input type="text" name="" value="">
+    </div>
+    
+    <div>
+        <label for="description">Description</label>
+        <textarea type="text" name="" value=""></textarea>
+    </div>
+    
+    <div>
+        <input type="submit" name="save_website" />
+    </div>
+    
+</form>
---
+++ new.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Web Development Resources</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+
+        <?php include 'form.php' ?>
+        
+    </body>
+</html>
