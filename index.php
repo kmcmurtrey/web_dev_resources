b525795ec11bd2b6e831ff2465bf852610aa6430
Korey McMurtrey <kmcmurtrey@gmail.com>
2015-06-16 18:39:18 -0400

    Add basic styles

--- index.php
+++ index.php
@@ -12,20 +12,26 @@ $websites = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html lang="en">
 <head>
     <title>Web Development Resources</title>
-
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+    <div class="container">
+    
+        <h1>Web Development Resources</h1>
 
-<ol>
-    <!-- loop through each website -->
-    <?php foreach( $websites as $website ) : ?>
-        <li>
-            <!-- output: <a href="__URL__">__TITLE__</a> by: __AUTHOR__ -->
-            <a href="<?php echo $website['url'] ?>"><?php echo $website['title'] ?></a>
-            <p>Description: <?php echo $website['description'] ?></p>
-        </li>
-    <?php endforeach; ?>
-</ol>
+        <div class="website-list">
+            <ol>
+                <!-- loop through each website -->
+                <?php foreach( $websites as $website ) : ?>
+                    <li>
+                        <!-- output: <a href="__URL__">__TITLE__</a> -->
+                        <a href="<?php echo $website['url'] ?>"><?php echo $website['title'] ?></a>
+                        <p>Description: <?php echo $website['description'] ?></p>
+                    </li>
+                <?php endforeach; ?>
+            </ol>
+        </div>
+    </div>
 
 <a href="new.php">New Website</a>
 </body>
