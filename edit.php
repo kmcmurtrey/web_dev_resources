32846d3aa502b3cbef31d7eca1b24ac151086996
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-06-18 06:34:18 -0400

    Edit.php: prepare and execute SQL statement to update website data on submit. Populate form fields with website data when user selects 'Edit.'

--- edit.php
+++ edit.php
@@ -13,13 +13,11 @@ if (!isset($_POST['save_website'])) {
         //prepare statement to find current website
         $stmt = $dbh->prepare("SELECT * FROM websites WHERE id = :id");
 
-        //run the SQL query
+        //execute SQL query
         $stmt->execute(array('id' => $_GET['id']));
 
         //set $website variable for the form fields
         $website = $stmt->fetch(PDO::FETCH_ASSOC);
-        print_r($website);
-        die();
 
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -28,7 +26,33 @@ if (!isset($_POST['save_website'])) {
 }
 
 //if the form was submitted (after editing), prepare query to update website's info in database
+if (isset($_POST['save_website'])) {
+    try {
+        //define input variables sent via POST
+        $url = $_POST['url'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
 
+        //prepare statement to update website's information
+        $stmt = $dbh->prepare("UPDATE websites SET url = :url, title = :title, description = :description WHERE id = :id");
+
+        //execute SQL update query
+        if ($stmt->execute(array(
+            'url' => $url,
+            'title' => $title,
+            'description' => $description,
+            'id' => $_GET['id']
+        ))) {
+            header('Location: index.php');
+        } else {
+            echo "Your changes were not saved to the database.";
+        }
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        die();
+    }
+}
 
 ?>
 
--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <form method="POST" class="website_form">
     <div>
         <label for="title">Title</label>
-        <input type="text" name="title" value="">
+        <input type="text" name="title" value="<?php echo $website['title']; ?>">
     </div>
     
     <div>
         <label for="url">URL</label>
-        <input type="text" name="url" value="">
+        <input type="text" name="url" value="<?php echo $website['url']; ?>">
     </div>
     
     <div>
         <label for="description">Description</label>
-        <textarea type="text" name="description" value=""></textarea>
+        <textarea type="text" name="description"><?php echo $website['description']; ?></textarea>
     </div>
     
     <div>
