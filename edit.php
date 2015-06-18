c052151d1295289d3efef9783b3e8739b2260da6
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-06-17 22:51:02 -0400

    On Edit.php, prepare and execute SQL statement to select database row that matches the selected website.

--- edit.php
+++ edit.php
@@ -2,6 +2,33 @@
 
 include 'global.php';
 
+//if the id wasn't passed, go to index.php
+if (!isset($_GET['id'])) {
+    header('Location: index.php');
+}
+
+//if the form wasn't submitted
+if (!isset($_POST['save_website'])) {
+    try {
+        //prepare statement to find current website
+        $stmt = $dbh->prepare("SELECT * FROM websites WHERE id = :id");
+
+        //run the SQL query
+        $stmt->execute(array('id' => $_GET['id']));
+
+        //set $website variable for the form fields
+        $website = $stmt->fetch(PDO::FETCH_ASSOC);
+        print_r($website);
+        die();
+
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        die();
+    }
+
+}
+
+//if the form was submitted (after editing), prepare query to update website's info in database
 
 
 ?>
