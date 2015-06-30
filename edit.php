a988432c11de76020e4596ddce92b862c98b4a0d
Korey McMurtrey <kmcmurtrey@gmail.com>
2015-06-30 19:17:24 -0400

    Refactors code, creating edit_website.php

--- edit.php
+++ edit.php
@@ -12,35 +12,9 @@ if (!isset($_POST['save_website'])) {
     include 'database/selected_website.php';
 }
 
-//if the form was submitted (after editing), prepare query to update website's info in database
+//if the form was submitted (after editing), prepare query to update database row
 if (isset($_POST['save_website'])) {
-    try {
-        //define input variables sent via POST
-        $url = $_POST['url'];
-        $title = $_POST['title'];
-        $category = $_POST['category'];
-        $description = $_POST['description'];
-
-        //prepare statement to update website's information
-        $stmt = $dbh->prepare("UPDATE websites SET url = :url, title = :title, description = :description, category = :category WHERE id = :id");
-
-        //execute SQL update query
-        if ($stmt->execute(array(
-            'url' => $url,
-            'title' => $title,
-            'description' => $description,
-            'category' => $category,
-            'id' => $_GET['id']
-        ))) {
-            header('Location: index.php');
-        } else {
-            echo "Your changes were not saved to the database.";
-        }
-
-    } catch (Exception $e) {
-        echo $e->getMessage();
-        die();
-    }
+    include 'database/edit_website.php';
 }
 
 ?>
