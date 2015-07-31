87d1fd951f400298fe169bc5f0451a09ffb568af
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-07-30 21:39:45 -0400

    Adds comments to index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,24 @@
 <?php
 
+//Uses Composer's autoloader, and includes a modal from Bootstrap.
 require '../vendor/autoload.php';
 include '../views/confirm_delete_modal.php';
 
-
+//Instantiates new WebsiteData, Paginator, and Votes classes.
 $website_list = new \WebDevResources\WebsiteData();
 $p = new \WebDevResources\Paginator();
 $votes = new \WebDevResources\Votes();
-$votes->checkForId();
+
+//Method calls to get the starting and ending websites to display in the current view, list of websites that will be displayed, and the category list.
 $start = $p->getStart();
 $end = $p->getEnd();
 $websites = $p->getWebsitesSubset($start, $end);
 $categories = $website_list->getAllCategories();
 
+//Checks for website Id's. If present, the vote count is increased.
+$votes->checkForId();
+
+//Instantiates Template class which renders the different views.
 $template = new \WebDevResources\Template('../views/base.phtml');
-//Render the content template we want, passing through the $websites and $categories variables so the template can access them.
+//Render the content template we want, passing through variables so the template can access them.
 $template->render('../views/index/index.phtml', ['websites' => $websites, 'categories' => $categories, 'p' => $p, 'votes' => $votes, 'website_list' => $website_list]);
